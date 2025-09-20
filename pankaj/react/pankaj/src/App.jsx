1ef80b0308a55f3e2e34c24f6e6e1bfa5a34a85c
Pankaj Pramanik <pankajpramanik002@gmail.com>
2025-09-20 13:10:33 +0530

    Pankaj (#3)
    * added components
    
    * added new file

---
+++ pankaj/react/pankaj/src/App.jsx
@@ -0,0 +1,24 @@
+
+import Name from './components/pankaj'
+import Number from './components/pankaj'
+import './App.css'
+import Add from './components/sonu'
+import Greet from './greet'
+import Sum from './components/sum'
+import Multi from './components/multipal'
+
+function App() {
+
+  return (
+    <>
+      <Name/>
+      <Number/>
+      <Add/>
+      <Greet/>
+      <Sum/>
+      <Multi/>
+    </>
+  )
+}
+
+export default App
