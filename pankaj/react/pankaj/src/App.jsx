f99020b9489fdab8e16b77cc6ac5e46f192bbe95
Pankaj-web861 <pankajpramanik002@gmail.com>
2025-09-20 13:02:21 +0530

    added components

---
+++ pankaj/react/pankaj/src/App.jsx
@@ -0,0 +1,23 @@
+
+import Name from './components/pankaj'
+import Number from './components/pankaj'
+import './App.css'
+import Add from './components/sonu'
+import Greet from './greet'
+import Sum from './components/sum'
+
+function App() {
+
+  return (
+    <>
+      <Name/>
+      <Number/>
+      <Add/>
+      <Greet/>
+      <Sum/>
+      
+    </>
+  )
+}
+
+export default App
---
+++ pankaj/react/pankaj/src/components/pankaj.jsx
@@ -0,0 +1,9 @@
+function Name(){
+    return(
+        <div>
+            <h1>WELCOME</h1>
+            
+        </div>
+    )
+}
+export default Name;
