7c2aca93923d69b9e5437a6fd66d76f3b3f1c903
DC <debayudh.maldaitbss@gmail.com>
2025-09-22 20:39:10 +0530

    Season 2 started

---
+++ czar/Explore-React/src/components/alert2.jsx
@@ -0,0 +1,13 @@
+function ALert2 (){
+
+    function handleAgain(){
+        alert("Season 2 started")
+    }
+    return(
+        <div>
+            <button onClick={handleAgain}>ALERT !</button>
+        </div>
+    )
+}
+
+export default ALert2
